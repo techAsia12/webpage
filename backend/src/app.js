f363c9fb7600cd6cca0c74225fac0a6d0960ae3c
Priyesh Chiplunkar <techasiamechatronics@gmail.com>
2025-03-27 16:45:15 +0530

    Refactor app.js for improved code consistency; update CORS origin handling and enhance formatting for better readability.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,20 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-const app=express()
+const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", 
-      "https://smartenergymeter.techasiamechatronics.com", 
-    ],
-    credentials: true, 
+    origin: process.env.ORIGIN,
+    credentials: true,
   })
 );
 
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.set("trust proxy",1);
+app.set("trust proxy", 1);
 
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
@@ -25,12 +22,12 @@ app.use((req, res, next) => {
   next();
 });
 //"C:\Users\priye\AndroidStudioProjects\SmartEnergyMeter\app\build\outputs\apk\debug\app-debug.apk"
-import  userRoute from "./routes/user.routes.js";
-import  adminRoute from "./routes/admin.routes.js";
-import {errorHandler} from "./middleware/errorHandling.middleware.js"
+import userRoute from "./routes/user.routes.js";
+import adminRoute from "./routes/admin.routes.js";
+import { errorHandler } from "./middleware/errorHandling.middleware.js";
 
-app.use('/api/user',userRoute);
-app.use('/api/admin',adminRoute);
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
 app.use(errorHandler);
 
 export default app;
